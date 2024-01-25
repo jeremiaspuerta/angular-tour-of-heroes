9a76d40a583218352dd775e0183ff150330f1a25
Jeremias Puerta <jeremiaspuerta12@gmail.com>
2024-01-25 16:52:13 -0300

    lesson 3

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../heroes/hero';
+
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css'
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+}
